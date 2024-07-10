c5aa5e0e3cd7f2f71a58a0a105d6a59a2a38f935
Lander Furusho Gerotto <lander.gerotto@gmail.com>
2024-07-10 11:15:24 -0300

    fuck

--- startup/db.js
+++ startup/db.js
@@ -3,19 +3,32 @@
 // vercel eu passei la pro index.js
 
 const mongoose = require("mongoose");
+const { Client } = require('pg')
 require('dotenv').config();
 
 const db_uri = process.env.MONGODB_CONNECT_URI;
 
+// async function connectToDB() {
+//   try {
+//     await mongoose.connect(db_uri, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log("Connected to MongoDB Atlas");
+//   } catch (error) {
+//     console.error("Error connecting to MongoDB Atlas:", error.stack);
+//   }
+// }
+
 async function connectToDB() {
+
+  const client = new Client (db_uri)
+
   try {
-    await mongoose.connect(db_uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Connected to MongoDB Atlas");
+    await client.connect();
+    console.log("Connected to CockroachDB");
   } catch (error) {
-    console.error("Error connecting to MongoDB Atlas:", error);
+    console.error("Error connecting to CockroachDB:", error.stack);
   }
 }
 
